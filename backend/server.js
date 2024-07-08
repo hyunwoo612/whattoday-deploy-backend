f4cab51eb53f9776604e8fae1b0483c470af1243
unknown <sky210120@daum.net>
2024-07-09 02:25:42 +0900

    login test 3

--- backend/server.js
+++ backend/server.js
@@ -165,7 +165,7 @@ app.post('/signup', async (req, res) => {
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post('/defaultlogin', async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
